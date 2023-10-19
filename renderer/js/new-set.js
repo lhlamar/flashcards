d7f8242e163e0f6a460c9ebb94942279bbe52219
lucas lamar <llamareo@gmail.com>
2023-10-18 20:34:54 -0500

    did some work on the new set page. not done yet but some good progess has been made. --still working on front end right now

--- renderer/js/new-set.js
+++ renderer/js/new-set.js
@@ -1,11 +1,23 @@
+let num_of_card = 2;
 
+const back_button = document.getElementById("back-button");
+const new_card_button = document.getElementById("new-item-button");
+const card = document.querySelector(".card-container");
+const card_list = document.querySelector(".flashcard-list-container");
 
+new_card_button.addEventListener("click", () => {
+  new_card();
+});
 
-const back_button = document.getElementById('back-button');
+back_button.addEventListener("click", () => {
+  ipcRenderer.send("open-home-window");
+});
 
+function new_card() {
+  num_of_card++;
+  const new_card = card.cloneNode(true);
+  const new_card_header = new_card.querySelector(".card-header");
+  new_card_header.innerHTML = `card ${num_of_card}`;
 
-
-back_button.addEventListener('click', () => {
-    console.log('test');
-    ipcRenderer.send('open-home-window');
-})
+  card_list.appendChild(new_card);
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain} = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require("path");
 
 let mainWindow;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -14,51 +14,50 @@ function createHomeWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
+      minWidth: 475,
     },
   });
   mainWindow.webContents.openDevTools();
 
-    // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-   mainWindow.loadFile(path.join(__dirname, '../renderer/home.html'));
+  // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadFile(path.join(__dirname, "../renderer/home.html"));
 }
 
 //create new card window
-ipcMain.on('open-new-set-window', () => {
-    // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-   mainWindow.loadFile(path.join(__dirname, '../renderer/new-set.html'));
+ipcMain.on("open-new-set-window", () => {
+  // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadFile(path.join(__dirname, "../renderer/new-set.html"));
 });
 
-ipcMain.on('open-home-window', () => {
+ipcMain.on("open-home-window", () => {
   // mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-   mainWindow.loadFile(path.join(__dirname, '../renderer/home.html'));
+  mainWindow.loadFile(path.join(__dirname, "../renderer/home.html"));
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.on("ready", () => {
   createHomeWindow();
   Menu.setApplicationMenu(null);
   // const mainMenu = Menu.buildFromTemplate(menu);
   // Menu.setApplicationMenu(mainMenu);
 
   // Remove variable from memory
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on("closed", () => (mainWindow = null));
 });
-
-
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
